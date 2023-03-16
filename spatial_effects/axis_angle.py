Bugfix; rotation vector updates; more tests:  - Fix SO3 to rvec conversion bug when norm(r) == pi  - Restrict angle of Rodrigues vectors to lie in [0,pi]  - Validate rotation matrix passed to SE3 constructor  - Improve test coverage over these topics

spatial_effects/axis_angle.py
@@ -7,6 +7,7 @@ from .common import reshape_nx3
 __all__ = (
     "rotate_axis_angle",
     "rrand",
+    "keep_north",
 )
 
 
@@ -57,9 +58,33 @@ def rotate_axis_angle(v, r):
     return v_rotated
 
 
+def keep_north(r: np.ndarray):
+    """Restrict the angle of a Rodrigues vector to have a positive
+    rotation in [0,pi] and a direction in the north hemisphere.
+    The sign of r is flipped when norm(r) = pi and certain component
+    conditions hold.
+
+    References
+    ----------
+    https://courses.cs.duke.edu/fall13/compsci527/notes/rodrigues.pdf
+    """
+    assert r.size == 3 and r.ndim == 1
+    theta = np.linalg.norm(r)
+    eps = np.finfo(float).eps
+    if abs(theta - pi) < eps:
+        rx, ry, rz = r
+        if rx < 0:
+            return -r
+        if abs(rx) < eps and ry < 0:
+            return -r
+        if abs(rx) < eps and abs(ry) < eps and rz < 0:
+            return -r
+    return r
+
+
 def rrand(n: int = 1) -> np.ndarray:
-    """Generate `n` random rotation vectors on the unit sphere whose norm
-    is a rotation angle in [0, 2pi).
+    """Sample `n` random rotation vectors from the upper half of a ball with
+    radius pi.
 
     Parameters
     ----------
@@ -70,8 +95,11 @@ def rrand(n: int = 1) -> np.ndarray:
     array containing n random axis-angle vectors normalized to random rotation angles
     """
 
-    # Get random rotation axes by sampling from an isotropic Gaussian
+    # Get random rotation axes by sampling from an isotropic Gaussian.
+    # Constrain directions to north hemisphere.
     vecs = np.random.randn(n, 3)
+    south = vecs[:, 2] < 0
+    vecs[south, 2] = -vecs[south, 2]
     norms = np.linalg.norm(vecs, axis=1)
 
     # Resample any points that are too close to zero for safe normalization
@@ -81,8 +109,8 @@ def rrand(n: int = 1) -> np.ndarray:
         norms[shorties] = np.linalg.norm(vecs[shorties], axis=1)
         shorties = np.isclose(norms, 0.0)
 
-    # Normalize to a random value in [0, 2pi)
-    vecs = 2 * pi * np.random.uniform(size=[n, 1]) * vecs / norms[:, np.newaxis]
+    # Normalize to a random value in [0, pi)
+    vecs = pi * np.random.uniform(size=[n, 1]) * vecs / norms[:, np.newaxis]
 
     if n == 1:
         vecs = vecs.ravel()

spatial_effects/conversions.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from .common import cross_product_matrix
 from .quaternion import qleft, qright, normalize, q_angle, q_axis, expq
+from .axis_angle import keep_north
 
 __all__ = (
     "hc",
@@ -312,9 +313,14 @@ def so3_to_rvec(R: np.ndarray) -> np.ndarray:
     -------
     r : ndarray - shape (3,)
         Rodrigues vector
+
+    Reference
+    ---------
+    "Vector Representation of Rotations" by Carlo Tomasi
+    https://courses.cs.duke.edu/fall13/compsci527/notes/rodrigues.pdf
     """
 
-    # Handle special case of symmetric R, such as R = I
+    # Handle special case of symmetric R
     if np.allclose(R - R.T, np.zeros((3, 3))):
         c = (np.trace(R) - 1) / 2  # this is cos(theta)
         if c == 1:
@@ -322,8 +328,10 @@ def so3_to_rvec(R: np.ndarray) -> np.ndarray:
             return np.zeros(3)
         if c == -1:
             # theta is odd*pi
-            v = R[:, 0]
-            return np.pi * v / np.linalg.norm(v)
+            # One way to find a nonzero column
+            col = np.argmax(np.sum(R + np.eye(3), axis=0))
+            v = R[:, col]
+            return keep_north(np.pi * v / np.linalg.norm(v))
 
     a, b, c = R[0]
     d, e, f = R[1]
@@ -337,6 +345,7 @@ def so3_to_rvec(R: np.ndarray) -> np.ndarray:
     u_norm = np.linalg.norm(u)
     if isclose(u_norm, 0.0):
         return np.zeros(3)
+
     return theta * u / u_norm
 
 

spatial_effects/tests/test_rotations.py
@@ -49,12 +49,16 @@ class RotationTests(unittest.TestCase):
         # a chi squared test comparing observed counts/bin (phi_hist) vs.
         # expected counts/bin (mu).
         num_bins = 10
-        vecs /= np.linalg.norm(vecs, axis=1)[:, np.newaxis]
+        norms = np.linalg.norm(vecs, axis=1)[:, np.newaxis]
+        vecs /= norms
         phis = np.arctan2(vecs[:, 1], vecs[:, 0])
         phi_hist, phi_bin_edges = np.histogram(phis, bins=num_bins, range=[-pi, pi])
         mu = num_samples / num_bins
         chi_squared = np.sum(1 / mu * (phi_hist - mu) ** 2)
         self.assertLess(chi_squared / num_bins, 3.0)
+
+        self.assertTrue(np.all(norms >= 0.0))
+        self.assertTrue(np.all(norms <= pi))
 
         # TODO: test that the polar distribution is proportional to sin(theta).
         # thetas = np.arccos(vecs[:, 2] / lengths)