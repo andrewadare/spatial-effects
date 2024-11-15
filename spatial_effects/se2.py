ab424f835be9b18cfad1877fd1c3eb9e004300e5
Andrew Adare <andrewadare@gmail.com>
2024-11-14 17:40:42 -0800

    Type annotations for SE2 methods

--- spatial_effects/se2.py
+++ spatial_effects/se2.py
@@ -1,3 +1,5 @@
+from typing import Union  # for python < 3.12
+
 import numpy as np
 
 from .common import in_so2
@@ -47,19 +49,19 @@ class SE2:
                 f"Received {len(args)}"
             )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.matrix)
 
     def __call__(self, x: np.ndarray) -> np.ndarray:
         """Apply this SE(2) transformation to x"""
         return self.R @ x + self.t
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if not isinstance(other, SE2):
             raise ValueError("Comparison with a non-SE2 object")
         return np.allclose(self.matrix, other.matrix)
 
-    def __add__(self, vec: np.ndarray):
+    def __add__(self, vec: np.ndarray) -> "SE2":
         """Returns an SE2 object updated by a 3 DOF vector [x, y, phi]."""
         if not isinstance(vec, np.ndarray) or vec.size != 3:
             raise ValueError(
@@ -69,7 +71,7 @@ class SE2:
         vec = vec.ravel()
         return SE2(self.matrix @ SE2(vec[:2], vec[2:]).matrix)
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> np.ndarray:
         """Returns a 3 DOF Euclidean vector [dx, dy, dphi]
         as the difference between two SE2 objects.
         """
@@ -77,7 +79,7 @@ class SE2:
             raise ValueError("Subtraction with a non-SE2 object")
         return SE2(other.inverse.matrix @ self.matrix).vec
 
-    def __mul__(self, other):
+    def __mul__(self, other: Union["SE2", np.ndarray]):
         """Matrix multiplication operator: if `other` is an SE2 object, returns
         a composed SE2 object. If `other` is an array of one or more points,
         the transformed point(s) will be returned.
@@ -90,32 +92,34 @@ class SE2:
             raise ValueError("* operator must be followed by an SE2 or array object.")
 
     @property
-    def inverse(self):
+    def inverse(self) -> "SE2":
         return SE2(-self.R.T @ self.t, self.R.T)
 
     @property
-    def matrix(self):
+    def matrix(self) -> np.ndarray:
+        """Returns 3x3 homogeneous transform matrix"""
         M = np.eye(3)
         M[:2, :2] = self._R
         M[:2, 2] = self._t
         return M
 
     @property
-    def t(self):
+    def t(self) -> np.ndarray:
         return self._t
 
     @t.setter
-    def t(self, t_):
+    def t(self, t_) -> None:
         self._t = t_
 
     @property
-    def R(self):
+    def R(self) -> np.ndarray:
         return self._R
 
     @R.setter
-    def R(self, R_):
+    def R(self, R_) -> None:
         self._R = R_
 
     @property
-    def vec(self):
+    def vec(self) -> np.ndarray:
+        """Returns [x, y, phi] vector"""
         return np.hstack([self._t, so2_angle(self._R)])
